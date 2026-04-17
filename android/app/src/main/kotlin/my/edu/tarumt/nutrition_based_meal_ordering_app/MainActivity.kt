e8e8df59bcb61961aa1c06776f6b9c77d0c8c961
W-Hin <chinwenghin@gmail.com>
2026-04-18 00:29:14 +0800

    stripe integration test

--- android/app/src/main/kotlin/my/edu/tarumt/nutrition_based_meal_ordering_app/MainActivity.kt
+++ android/app/src/main/kotlin/my/edu/tarumt/nutrition_based_meal_ordering_app/MainActivity.kt
@@ -1,5 +1,5 @@
 package my.edu.tarumt.nutrition_based_meal_ordering_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
